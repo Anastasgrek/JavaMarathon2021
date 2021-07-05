3106168039ae75a8fcb1402f784acf7cb0b3be38
Anastasgrek <akonstantinidi3@gmail.com>
2021-07-05 23:06:19 +0300

    complete day5 tasks

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -1,7 +1,27 @@
 package day5;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Task1 {
     public static void main(String[] args) {
 
+        Car car = new Car();
+
+        car.setYear(2021);
+        car.setColor("Green");
+        car.setModel("Kia");
+
+        System.out.println(car.getYear());
+        System.out.println(car.getColor());
+        System.out.println(car.getModel());
+
+        Motorbike motorbike = new Motorbike(2020, "Red", "Honda");
+
+        System.out.println(motorbike.getYear());
+        System.out.println(motorbike.getColor());
+        System.out.println(motorbike.getModel());
+
     }
+
+
 }
